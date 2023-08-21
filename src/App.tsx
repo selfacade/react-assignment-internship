f62a2665d2732f609bdeb626e6bda8132b05f105
Mr.JaineekP <jaineekp@gmail.com>
2023-08-22 03:53:32 +0530

    Completed the responsive form UI of with UseState hook for collecting the data

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import {
+  Box,
+  Grid,
+  Paper,
+  TextField,
+  Button,
+  CardMedia,
+  useMediaQuery,
+} from "@mui/material";
+import WelcomeImage from "../public/assets/Welcome!.png";
+import Typography from '@mui/material/Typography';
+
+
+const UserForm = () => {
+
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+
+  const [formData, setFormData] = useState({
+    name: "",
+    phoneNumber: "",
+    email: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Save the data to local storage here
+    // Redirect user to the second page
+  };
+
+  return (
+    <Box
+        width="auto"
+        display={isNonMobileScreens ? "flex" : "block"}
+
+      style={{
+        // flexWrap: "wrap",
+        width: "100%",
+        height: "100%",
+        padding: "0",
+        boxShadow:
+          "0px 3px 3px -2px rgba(0,0,0,0.2), 0px 3px 4px 0px rgba(0,0,0,0.14), 0px 1px 8px 0px rgba(0,0,0,0.12)",
+      }}
+    >
+      <Grid item xs={6} style={{ display: "flex", flexDirection: "row" }} flexBasis={isNonMobileScreens ? "50%" : undefined}>
+        <CardMedia
+          style={{ objectFit: "cover" }}
+          component="img"
+          height="100%"
+          width="100%"
+          image={WelcomeImage}
+          alt="GFG Logo"
+        />
+      </Grid>
+      <Grid item xs={6} flexBasis={isNonMobileScreens ? "50%" : undefined}>
+        <Paper elevation={3} style={{ padding: "20px", boxShadow: "none" }}>
+        <Typography variant="h4" align="center" fontWeight="bold">
+        Get in touch with us
+      </Typography>
+          <form
+            onSubmit={handleSubmit}
+            style={{ height: "85vh", boxShadow: "none" }}
+          >
+            <TextField
+              fullWidth
+              label="Name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              fullWidth
+              label="Phone Number"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              fullWidth
+              label="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              margin="normal"
+              variant="outlined"
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </form>
+        </Paper>
+      </Grid>
+    </Box>
+  );
+};
+
+export default UserForm;
